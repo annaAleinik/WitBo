a89906db9f7489e8e309cbd27dccce3155c7b872
Admin <annaaleinik1807@gmail.com>
2018-05-15 17:50:23 +0300

    add assets image and picker

--- Proj/Controllers/RegistrationVC.swift
+++ Proj/Controllers/RegistrationVC.swift
@@ -8,31 +8,35 @@
 
 import UIKit
 
-class RegistrationVC: UIViewController , UITextFieldDelegate {
-
+class RegistrationVC: UIViewController , UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+   
+    
+    @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
-    @IBOutlet weak var langField: UITextField!
     @IBOutlet weak var repiatPassword: UITextField!
     
+    let langSourse = LanguageSourse.shared.dictLang
+    var flagArr = LanguageSourse.shared.dictFlag
+    var language: String? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        
         nameField.placeholder = "Name"
         emailField.placeholder = "Email"
         passwordField.placeholder = "Password"
-        langField.placeholder = "Lang"
         repiatPassword.placeholder = "Repiat password"
         
        self.nameField.delegate = self
        self.emailField.delegate = self
       self.passwordField.delegate = self
-       self.langField.delegate = self
        self.repiatPassword.delegate = self
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+
     }
 
     
@@ -41,9 +45,13 @@ class RegistrationVC: UIViewController , UITextFieldDelegate {
         let name = nameField.text
         let email = emailField.text
         let password = passwordField.text
-        let lang = langField.text
         
-        APIService.sharedInstance.postRegistration(name: name!, email: email!, password: password!, lang: lang!)
+       guard let userEmail = email else {return}
+        guard let userName = name else {return}
+        guard let userPass = password else {return}
+        guard let userLang = self.language else {return}
+        
+        APIService.sharedInstance.postRegistration(name: userName, email: userEmail, password: userPass, lang: userLang)
         
         let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
         let vc = storyBoard.instantiateViewController(withIdentifier: "autentificattionControl")
@@ -71,13 +79,38 @@ class RegistrationVC: UIViewController , UITextFieldDelegate {
             repiatPassword.becomeFirstResponder()
         }else if textField == repiatPassword{
             textField.resignFirstResponder()
-            langField.becomeFirstResponder()
-        }else if textField == langField {
-            textField.resignFirstResponder
         }
         return true
     }
     
    
+    //MARK: -- UIPickerViewDelegate
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.langSourse.count
+    }
 
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.langSourse[row].name
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.language = self.langSourse[row].code
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+
+        let flagImage = UIImageView()
+        flagImage.image = flagArr[row].img
+        
+//
+//        let myImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+//        var myImageView = UIImageView()
+//        let myView = CGRect(x: 0.0, y: 0.0, width: pickerView.bounds.width - 30, height: 60.0)
+        
+                return flagImage
+    }
 }
